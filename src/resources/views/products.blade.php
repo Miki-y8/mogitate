887ddcea2277d524a3d29bddb2d7ba34684530df
miki <mi7893451dmgjs8@gmail.com>
2025-03-18 11:01:00 +0900

    画像

--- src/resources/views/products.blade.php
+++ src/resources/views/products.blade.php
@@ -5,10 +5,11 @@
 @endsection
 
 @section('products')
-<div class="product">
-  <h2 class="product__heading product__heading">商品一覧</h2>
-  <div class="product-form__inner">
-    <input class="product-form__btn btn" type="submit" value="商品を追加">
+<div class="products">
+  <h2 class="product__heading products__heading">商品一覧</h2>
+  <div class="products-form__inner">
+    <input class="products-form__btn btn" type="submit" value="商品を追加">
     <form action="entry" method="post">
 </div>
     </div>
+    
