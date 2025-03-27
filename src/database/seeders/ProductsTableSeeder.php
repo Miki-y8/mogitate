ca327a981eb9051bfea319ee3ceb271e8280c595
miki <mi7893451dmgjs8@gmail.com>
2025-03-27 13:37:42 +0900

    seed

--- src/database/seeders/ProductsTableSeeder.php
+++ src/database/seeders/ProductsTableSeeder.php
@@ -16,7 +16,10 @@ class productsTableSeeder extends Seeder
     {
     $param = [
       'name' => 'kiwi',
-      'price' => 800
+      'price' => 800,
+      'image' => '',
+      'description' => 'キウイは甘みと酸味のバランスが絶妙なフルーツです。ビタミンCなどの栄養素も豊富のため、美肌効果や疲労回復効果も期待できます。もぎたてフルーツのスムージーをお召し上がりください！',
+
         ];
     DB::table('products')->insert($param);
 
